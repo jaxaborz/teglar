5cd4cda5c9c504a91d45f5ed10965080c5fe96cd
jaxaborz <107840287+jaxaborz@users.noreply.github.com>
2022-06-27 17:08:39 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+<skript type= 
+ //let age= 30 // data - type - number
+//let mayName='javohir' // data - type - string
+//let bool = 'false' // data - type - boolean  
+// object
+//{great:"salom",age;15,brotherAge;'12'}
+//Boolean
+//true //tugri
+//false//notugri
+//undefined
+//undefined//mavjud emas
+//<br>qator tashlash
+//var uzgaruvchi//
